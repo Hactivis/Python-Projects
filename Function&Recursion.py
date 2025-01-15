23f2e598b7faaf28f7f30d28cce46bfe3662e39f
Rishabh Sharma <147262906+Hactivis@users.noreply.github.com>
2025-01-15 13:57:37 +0530

    Add files via upload

---
+++ Function&Recursion.py
@@ -0,0 +1,50 @@
+
+# Function Definition
+# def avg():
+#     a = int(input("Enter your number:"))
+#     b = int(input("Enter your number:"))
+#     c = int(input("Enter your number:"))
+
+#     average = (a + b + c)/300
+
+#     print(average)
+
+# avg()   # Function Call
+# print("Thank You!")
+# avg()
+# print("Thank You!")
+# avg()
+# print("Thank You!")
+# avg()
+# avg()
+
+# Quiz
+
+# def day():
+#     user = input("Enter Username:")
+#     print("Good Day!")
+
+# day()
+
+# Arguments
+# def GoodDay(name, ending="Thank you"):
+#     print("Good Day, " + name)
+#     print(ending)
+#     return a
+
+# a = int(input("Enter a number:"))
+
+
+# GoodDay("Rishabh" )
+# GoodDay("Shubham")
+# GoodDay("Varun", "Thanks")
+
+# Recursion
+
+# def factorial(n):
+#     if(n == 1 or n == 0):
+#         return 1
+#     return n * factorial(n-1)
+    
+# n = int(input("Enter a number:"))
+# print(f"The factorial of this number is: {factorial(n)}")
